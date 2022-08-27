ac2e9bb2f94642d477c7437fb20e8046dc3a59f5
savio-2-lopes <savioaugulopes@gmail.com>
2022-08-27 10:05:30 -0300

    Change Docker environment php

---
+++ app/index.php
@@ -0,0 +1,3 @@
+<?php
+echo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iusto eum alias vitae nesciunt animi beatae accusantium esse iure quia laudantium explicabo, nulla facere. Magnam facere voluptas doloribus nulla quasi?');
+?>
