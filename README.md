0e485a3cad1ff04c45ae94e5f21b47c158962e6f
savio-2-lopes <savioaugulopes@gmail.com>
2022-08-27 21:24:48 -0300

    Update environment Docker and add composer

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ambiente Docker PHP Apache MySQL
+
+### Rodando o servidor
+sudo docker compose up --build
+
+### Criando projeto Laravel
+composer create-project laravel/laravel .
--- app/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iusto eum alias vitae nesciunt animi beatae accusantium esse iure quia laudantium explicabo, nulla facere. Magnam facere voluptas doloribus nulla quasi?');
-?>
